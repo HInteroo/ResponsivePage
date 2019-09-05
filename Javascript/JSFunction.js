22a9a30b16be6ac8728096e2b4832086cfbce1bf
Max Juarez <maxjuarez69@gmail.com>
2019-09-05 18:00:13 -0400

    Update JSFunction.js

--- Javascript/JSFunction.js
+++ Javascript/JSFunction.js
@@ -1,3 +1,37 @@
-$(document).ready(function(){
-  console.log("bannerContent :"+$(".bannerContent").width()+"x"+$(".bannerContent").height()+"px");
+
+$(window).on('load', function () {
+  $('#rightButton').click(function(){             //Clicking on the -> arrow
+    var activeSlide = "#"+ $(".active").attr('id');
+    if($(activeSlide).next().addClass("active YesOpacity").length) {
+      addRemoveClasses (activeSlide);
+    }
+    else{
+      locationOfIndex(activeSlide,"First");
+    }
+  });
+  $('#leftButton').click(function(){                //Clicking on the <- arrow
+    var activeSlide = "#"+ $(".active").attr('id');
+    if($(activeSlide).prev().addClass("active YesOpacity").length) {
+      addRemoveClasses (activeSlide);
+      }
+    else{
+      locationOfIndex(activeSlide,"Last");
+    }
+  });
 });
+function locationOfIndex(idActive, index){
+  $(idActive).removeClass("active YesOpacity");
+  $(idActive).removeClass("NoOpacity");
+  $("li:"+index).addClass("active YesOpacity");
+  setTimeout(function(){
+    $("li:"+index).removeClass("NoOpacity")
+  }, 500);
+}
+
+function addRemoveClasses (idActive){
+  $(idActive).removeClass("active YesOpacity");
+  $(idActive).addClass("NoOpacity");
+  setTimeout(function(){
+    $(idActive).removeClass("NoOpacity");
+  }, 500);
+}
